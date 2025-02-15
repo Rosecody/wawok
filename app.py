e3718898b7360622f1110179bd0145cee6e0bc86
Rosemary <nandaevos0@gmail.com>
2025-02-15 14:59:38 +0700

    WAWOK KAU

---
+++ app.py
@@ -0,0 +1,5 @@
+# aplikasi umur
+tahun_user = int(input("Masukkan tahun lahir anda: "))
+tahun_sekarang = 2025
+
+print(f"umur kamu di tahun {tahun_sekarang} adalah {tahun_sekarang - tahun_user} tahun.")
---
+++ main.py
@@ -0,0 +1,4 @@
+import datetime as dt
+
+hari_ini = dt.date.today()
+print(hari_ini)  # Output: 2024-03-18
